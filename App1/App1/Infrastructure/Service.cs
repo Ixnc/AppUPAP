8bc259854d7318c95f0ecdce5c6789ec18deea30
isaac octavio ortiz <onix3.050@outlook.com>
2018-10-25 15:07:05 -0500

    Update App1/App1/Infrastructure/Service.cs

--- App1/App1/Infrastructure/Service.cs
+++ App1/App1/Infrastructure/Service.cs
@@ -9,33 +9,6 @@ namespace App1.Infrastructure
 {
     public class Service
     {
-        public async Task<Response> CheckConnection()
-        {
-            if (!CrossConnectivity.Current.IsConnected)
-            {
-                return new Response
-                {
-                    IsSuccess = false,
-                    Message = "Please turn on your internet settings.",
-                };
-            }
-
-            var isReachable = await CrossConnectivity.Current.IsRemoteReachable(
-                "google.com");
-            if (!isReachable)
-            {
-                return new Response
-                {
-                    IsSuccess = false,
-                    Message = "Check you internet connection.",
-                };
-            }
-
-            return new Response
-            {
-                IsSuccess = true,
-                Message = "Ok",
-            };
-        }
+        
     }
 }
